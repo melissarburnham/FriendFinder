1c6495fa27e0f07bb43a24e34b91063cf3fd1057
Your Name <melissarburnham@gmail.com>
2018-04-12 20:01:39 -0400

    initial commit of skeleton

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
