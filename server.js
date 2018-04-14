1e44673b80288a37c1dd1e3b1fb2d517d774e43f
Your Name <melissarburnham@gmail.com>
2018-04-14 16:41:50 -0400

    front end layout

--- server.js
+++ server.js
@@ -6,3 +6,31 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+
+var friends = [
+    {
+      name: "Melissa",
+      picture: "9198675309",
+      scores: [],
+    },
+    {
+      name: "Melissa",
+      picture: "9198675309",
+      scores: []
+    },
+    {
+      name: "Melissa",
+      picture: "9198675309",
+      scores: []
+    }
+  ];
+
+app.get("/", function(req, res) {
+res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/survey", function(req, res) {
+res.sendFile(path.join(__dirname, "survey.html"));
+  });
+
