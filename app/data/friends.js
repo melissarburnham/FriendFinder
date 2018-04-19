51b9bb43cf72d259417c5e80f760671540f17464
Your Name <melissarburnham@gmail.com>
2018-04-19 17:28:51 -0400

    modal working

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,17 +1,17 @@
 var friendsList = [
     {
       name: "Edward Cullen",
-      picture: "https://goo.gl/images/y9zahx",
+      picture: "https://orig00.deviantart.net/7a63/f/2009/331/5/6/edward_cullen_collage_by_alfabetagama.jpg",
       scores: ["1","1","1","1","1","1","1","1","1","1"],
     },
     {
       name: "Ellen DeGeneres",
-      picture: "https://goo.gl/images/DJtjfR",
+      picture: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Ellen_DeGeneres_2011.jpg/402px-Ellen_DeGeneres_2011.jpg",
       scores: ["2","2","2","2","2","2","2","2","2","2"]
     },
     {
       name: "John Krasinski",
-      picture: "https://goo.gl/images/1F9UTv",
+      picture: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/John_Krasinski_and_Josh_Wood_%28cropped%29.jpg/416px-John_Krasinski_and_Josh_Wood_%28cropped%29.jpg",
       scores: ["3","3","3","3","3","3","3","3","3","3"]
     }
   ];
