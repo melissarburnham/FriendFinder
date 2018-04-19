09f800e115114edcadb204111f4514048dffaa32
Your Name <melissarburnham@gmail.com>
2018-04-18 21:40:32 -0400

    beginning workings of math logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,7 +4,7 @@ var friends = require('../data/friends.js');
 
 module.exports = function(app) {
     //all friends
-	app.get('/api/friends', function(req, res) {
+	app.get("/api/friends", function(req, res) {
 		res.json(friends);
     });
 
@@ -12,10 +12,50 @@ module.exports = function(app) {
         //loop through friendListArray.score, add up all scores, 
         //and subtract from sum of newFriend array (look up absolute)
     app.post("/api/friends", function(req, res) {
-        var newFriendArray = req.body;
-        console.log(newFriendArray);
-        friends.push(newFriendArray);
-        res.json(newFriendArray);
-      });
-    
+        var newUser = req.body;
+        var newScore = 0;
+        var newScoreArray= [];
+        var oldScoresArray =[];
+        var newUserScores = newUser.scores;
+        var oldScoreSum;
+        // console.log(newUser.scores);
+        res.send(newUser);
+
+        for(var i = 0; i < newUserScores.length; i++){
+            //turns all scores into integers
+            newUserScores[i] = +newUserScores[i];
+            newScoreArray.push(newUserScores[i]);
+        } 
+        //add all numbers in newScoreArray
+        var newScoreSum = newScoreArray.reduce(add, 0);
+        function add(a, b) {
+        return a + b;
+        }
+
+
+        friends.forEach(element => {
+            oldScoreSum = element.scores.reduce(add,0);
+            function add(a, b) {
+                return parseInt(a) + parseInt(b);
+            }
+            oldScoresArray.push(oldScoreSum);
+        });
+
+        // oldScoresArray.forEach(element => {
+        //     oldScoresArray = +oldScoresArray;                                    
+        // });
+
+            
+        
+
+
+        console.log("NEW SUM:" + newScoreSum); 
+        console.log("NEW:" + newScoreArray);
+        console.log("OLD:" + oldScoresArray);
+        
+        // res.send(bestMatch);
+        friends.push(newUser);
+     });
 }
+
+// 
